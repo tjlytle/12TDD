cbef61e7f6d41030f2193fdfe8a257c1c9356c0c
Tim Lytle <tim@timlytle.net>
2012-12-26 23:21:05 -0500

    Solution to Day 1.

--- src/day1/Sequence.php
+++ src/day1/Sequence.php
@@ -1,5 +1,68 @@
 <?php
 class Sequence
 {
+    protected $values = array();
     
+    public function __construct($values)
+    {
+        $this->values = $values;
+    }
+    
+    /**
+     * Essentially min(), but that's not testing much.
+     */
+    public function min()
+    {
+        $min = null;
+        foreach($this->values as $value){
+            if($value < $min OR is_null($min)){
+                $min = $value;
+            }
+        }
+
+        return $min;
+    }
+    
+    /**
+     * Essentially max(), but what fun would that be?
+     */
+    public function max()
+    {
+        $max = null;
+        foreach($this->values as $value){
+            if($value > $max OR is_null($max)){
+                $max = $value;
+            }
+        }
+        
+        return $max;
+    }
+
+    /**
+     * Seems like there should be a built-in function to do this.
+     */
+    public function count()
+    {
+        $count = 0;
+        reset($this->values);
+        while(!is_null(key($this->values))){
+            $count++;
+            next($this->values);
+        }
+        
+        return $count;
+    }
+    
+    /**
+     * Not using array_sum(), just to make it interesting.
+     */
+    public function average()
+    {
+        $sum = 0;
+        foreach($this->values as $value){
+            $sum += $value;
+        }
+        
+        return $sum/$this->count();
+    }
 }
