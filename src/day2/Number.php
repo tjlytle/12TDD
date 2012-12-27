63c440fc795d43d29c912d8ac40c0598111badea
Tim Lytle <tim@timlytle.net>
2012-12-27 01:08:12 -0500

    Solution for Day 2.

--- src/day2/Number.php
+++ src/day2/Number.php
@@ -1,15 +1,161 @@
 <?php
 class Number
 {
+    protected $ones = array(
+        0 => '',
+        1 => 'one', 
+        2 => 'two',
+        3 => 'three',
+        4 => 'four',
+        5 => 'five',
+        6 => 'six',
+        7 => 'seven',
+        8 => 'eight',
+        9 => 'nine'
+    );
+        
+    protected $teens = array(
+        0 => 'ten',
+        1 => 'eleven',
+        2 => 'twelve',
+        3 => 'thirteen',
+        5 => 'fifteen',
+    );
+
+    protected $tens = array(
+        0 => '',
+        2 => 'twenty',
+        3 => 'thirty',
+        4 => 'forty',
+        5 => 'fifty',
+        6 => 'sixty',
+        7 => 'seventy',
+        8 => 'eighty',
+        9 => 'ninety'
+    );
+    
+    protected $thousands = array(
+        0 => '',
+        1 => 'thousand',
+        2 => 'million',
+        3 => 'billion',
+        4 => 'trillion'
+    );
+            
     protected $value;
+    
     
     public function __construct($value)
     {
         $this->value = $value;
     }
     
+    /**
+     * Convert the value to a string.
+     */
     public function __toString()
     {
-        return (string) $this->value;
+        //going to group things by thousands
+        $words = array();
+        //reverse the number, then split into 'thousands' (groups of three)
+        foreach(str_split(strrev($this->value), 3) as $position => $number){
+            //reverse the number again, so it's ordered correctly
+            $number = strrev($number);
+            //get the words for that group, passing the thousands place
+            $words[] = trim($this->getThousands($number, $position));
+        }
+        //reverse again to get the groups ordered, and add ','
+        return implode(', ', array_reverse($words));
+    }
+    
+    /**
+     * Get the word for ones place of th egiven number.
+     * @param string|int $number
+     */
+    protected function getOnes($number)
+    {
+        return $this->ones[$this->getPlace($number, 1)];
+    }
+
+    /**
+     * Get the word for the tens place, it's expected to include all lower
+     * places.
+     * @param string|int $number
+     */
+    protected function getTens($number)
+    {
+        //need the values for both tens and ones
+        $ones = $this->getPlace($number, 1);
+        $tens = $this->getPlace($number, 2);
+        
+        //special case for teens (they're special after all)
+        if(1 == $tens){
+            //is there a special format for the number?
+            if(isset($this->teens[$ones])){
+                return $this->teens[$ones];
+            }
+            
+            //all others just append 'teen' to the word for the ones place
+            return $this->getOnes($ones) . 'teen';
+        }
+        
+        //non-teens - get the tens plac and add the ones (trim for '0')
+        return trim($this->tens[$tens] . ' ' . $this->getOnes($number));
+    }
+
+    /**
+     * Get the word for the hundreds places, expect it includes the lower 
+     * places.
+     * @param string|int $number
+     */
+    protected function getHundreds($number)
+    {
+        //hundreds is the same as the ones place
+        $hundreds = $this->getOnes($this->getPlace($number, 3));
+        $tens = $this->getTens($number);
+
+        //this could probably be more elegant, just assemble the words checking
+        //to see if there is a word for the given place
+        $words = '';
+        if($hundreds){
+            $words .= $hundreds . ' hundred';
+        }
+        
+        //only use and when connecting hundreds and tens
+        if($hundreds AND $tens){
+            $words .= ' and ';
+        }
+        
+        if($tens){
+            $words .= $tens;
+        }
+        
+        return trim($words);
+    }
+    
+    /**
+     * Get the word for the thousands, adding the lower places - offest refers 
+     * to the place.
+     * @param string|int $number
+     * @param int $offset
+     */
+    protected function getThousands($number, $offset = 0)
+    {
+        //add the thousands word (if it's set), trimming for the '0' case
+        return trim($this->getHundreds($number) . ' ' . $this->thousands[$offset]);
+    }
+    
+    /**
+     * Utility function to get a int value for a specific place.
+     * @param strin|int $number
+     * @param int $position
+     */
+    protected function getPlace($number, $position)
+    {
+        if($position > strlen($number)){
+            return 0;
+        }
+
+        return substr($number, $position*-1, 1);
     }
 }
--- tests/day2/Test.php
+++ tests/day2/Test.php
@@ -34,13 +34,20 @@ class Test extends PHPUnit_Framework_TestCase
     public function dataset()
     {
         return array(
+            //given test cases
             array(99, 'ninety nine'),
             array(300, 'three hundred'),
             array(310, 'three hundred and ten'),
             array(1501, 'one thousand, five hundred and one'),
             array(12609, 'twelve thousand, six hundred and nine'),
             array(512607, 'five hundred and twelve thousand, six hundred and seven'),
-            array(43112603, 'forty three million, one hundred and twelve thousand, six hundred and three')
+            array(43112603, 'forty three million, one hundred and twelve thousand, six hundred and three'),
+            //testing some extra cases
+            array(111, 'one hundred and eleven'),
+            array(12, 'twelve'),
+            array(17, 'seventeen'),
+            array(1001, 'one thousand, one'), //not sure - should this be 'and one'?
+            array(101, 'one hundred and one')
         );
     }
 }
