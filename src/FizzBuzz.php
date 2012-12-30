89921011b1e152f5e3b53f0d761bc2f737dcd21c
Tim Lytle <tim@timlytle.net>
2012-12-30 00:44:52 -0500

    Working code for Day 5

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -1,4 +1,27 @@
 <?php
 class FizzBuzz
 {
+    public function getSequence($max)
+    {
+        $output = '';
+        for($i = 1; $i <= $max; $i++){
+            $line = '';
+            
+            if(0 == $i%3){
+                $line .= 'Fizz';
+            }
+            
+            if(0 == $i%5){
+                $line .= 'Buzz';
+            }
+            
+            if(empty($line)){
+                $line = $i;
+            }
+            
+            $output .= $line . PHP_EOL;
+        }
+        
+        return trim($output);
+    }
 }
